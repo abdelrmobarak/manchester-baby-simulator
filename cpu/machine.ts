9e317132985b48eefc29cb2d4cc524cacb43a3d4
Abdel-Rahman Mobarak <abdelrmobarak@gmail.com>
2025-10-23 22:20:21 -0400

    final

--- cpu/machine.ts
+++ cpu/machine.ts
@@ -3,15 +3,15 @@ import { encode } from './ops';
 import { Op } from './types';
 
 const machine = newMachine();
-
+// Write your machine instructions here
 machine.memory[0] = encode(Op.LDN, 10);
 machine.memory[1] = encode(Op.SUB, 11);
 machine.memory[2] = encode(Op.STO, 12);
 machine.memory[3] = encode(Op.STP, 0);
 
-machine.memory[10] = 5;
-machine.memory[11] = 3;
-machine.memory[12] = 0;
+machine.memory[10] = -5; // Enter your initial value
+machine.memory[11] = 3; // Enter your other value 
+machine.memory[12] = 0;  
 
 let steps = 0;
 while (machine.running && steps < 100) {
@@ -19,6 +19,7 @@ while (machine.running && steps < 100) {
     steps++;
 }
 
+//Logging the final results
 console.log("A =", machine.A);
 console.log("M12 =", machine.memory[12]);
 console.log("CI =", machine.CI);
